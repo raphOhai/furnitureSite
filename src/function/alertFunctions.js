74a413701a43b0392121240acdfd022f1c004a35
raphaelohai5 <sameleno6@gmail.com>
2023-07-08 00:40:44 +0100

    fixed

--- src/function/alertFunctions.js
+++ src/function/alertFunctions.js
@@ -10,7 +10,7 @@ export const ShowAlert = () => {
 export const AnimateOnScroll = (id) => {
   const ShowPriceLables = () => {
     const InfoBox = document.getElementById(id);
-    if (window.scrollY >= 150) {
+    if (window.scrollY >= 120) {
       InfoBox.classList.add("show");
     } else {
       InfoBox.classList.remove("show");
